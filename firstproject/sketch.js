796f0cba999e17a766cc19038f37babcad4109b2
EdgyPirate <45469463+EdgyPirate@users.noreply.github.com>
2019-09-09 14:11:34 -0600

    linefaart

--- firstproject/sketch.js
+++ firstproject/sketch.js
@@ -10,8 +10,28 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+let redChangeAmount = 1;
+let redAmount = 0;
+background(220)
+
 function draw() {
-  background(220);
-  Fill(0);
-  ellipse(mouseX, mouseY, 100, 100)
+  fill(redAmount ,255 ,0 );
+  
+  if (mouseIsPressed){
+    if(mouseButton === LEFT)
+    //noCursor();
+    noStroke();
+    ellipse(mouseX, mouseY, 25, 25);
+    redAmount += redChangeAmount;
+
+    if (redAmount >= 256 || redAmount <= 0) {
+      redChangeAmount *= -1;
+    }
+    //else if(mousebutton === CENTER)
+    
+  }
+
+
+
+  console.log(redAmount);
 }
---
+++ lineart/sketch.js
@@ -0,0 +1,20 @@
+// Linearts
+// Blazingsky Carrier 
+// Sep 09, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(25);
+}
+
+function draw() {
+  line(mouseX, mouseY, pmouseX, pmouseY,);
+  
+
+
+}
+
