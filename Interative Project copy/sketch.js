b8c10c6c367126bb2d3f370ebf289978b5c5d79c
EdgyPirate <45469463+EdgyPirate@users.noreply.github.com>
2019-09-19 14:15:50 -0600

    piano code

---
+++ Interative Project copy/sketch.js
@@ -0,0 +1,21 @@
+// Orange Eating
+// Blazingsky Carrier 
+// Sep 13, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+//var audio = new Audio('assets/test.mp3');
+
+// preload() runs once //
+function preload(){
+}
+
+// setup() waits untill preload() is done //
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw(){
+  background(125);
+}
--- Interative Project/sketch.js
+++ Interative Project/sketch.js
@@ -4,51 +4,60 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let x, y;
-
+let gangplank;
 let c;
-//var audio = new Audio('assets/test.mp3');
-var gangplank = ["assets/gangplank.png"];
+let mySound;
 
 // preload() runs once //
 function preload(){
-  gangplank = loadImage("assets/gangplank.png");
-  //soundFormats('mp3', 'oog');
+  //gangplank = loadImage('assets/gangplank.png');
+  sound = ('assets/noteA.mp3');
 }
 
 // setup() waits untill preload() is done //
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height /2;
-  background(125);
-  image(gangplank,x,y);
 }
 
 function draw(){
   background(125);
-  imageMode(CENTER);
-  image(gangplank,x,y,50,50);
+  keyboardnotes();
 }
 
 function windowResized(){
   setup();
 }
 
-// plays audio on keypress //
-function keyPressed(){
-  if (key === "w"){
-    (y += -10)
-  }
-  if (key === "s"){
-    (y += 10)
-  }
-  if (key === "a"){
-      (x += -10)
-  }
-  if (key === "d"){
-      (x += 10)
+// corresponds with a sound of the piano and shows the note pressed on the screen
+function keyboardnotes(){
+  if (keyIsPressed);{
+    if (key === 'a');{
+      noteA.play();
+      text(key, 33, 65);
+    }
+    if (key === 'b');{
+      noteB.play();
+      text(key, 33, 65);
+    }
+    if (key === 'c');{
+      noteC.play();
+      text(key, 33, 65);
+    }
+    if (key === 'd');{
+      noteD.play();
+      text(key, 33, 65);
+    }
+    if (key === 'e');{
+      noteE.play();
+      text(key, 33, 65);
+    }
+    if (key === 'f');{
+      noteF.play();
+      text(key, 33, 65);
+    }
+    if (key === 'g');{
+      noteG.play();
+      text(key, 33, 65);
+    }
   }
 }
-// if (key === "w");
-//  audio.play();ss
