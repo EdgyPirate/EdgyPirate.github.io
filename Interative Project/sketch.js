372614233e1430aedbc6165060ae1b35e63499ad
EdgyPirate <45469463+EdgyPirate@users.noreply.github.com>
2019-09-24 14:11:48 -0600

    interactive project keys
    sound works with keyboard keys a - g

--- Interative Project/sketch.js
+++ Interative Project/sketch.js
@@ -1,63 +1,62 @@
-// Orange Eating
+// Keyboard
 // Blazingsky Carrier 
 // Sep 13, 2019
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
-let gangplank;
-let c;
-let mySound;
 
-// preload() runs once //
 function preload(){
-  //gangplank = loadImage('assets/gangplank.png');
-  sound = ('assets/noteA.mp3');
+  soundFormats('mp3')
+  noteA = loadSound('assets/noteA.mp3')
+  noteB = loadSound('assets/noteB.mp3')
+  noteC = loadSound('assets/noteC.mp3')
+  noteD = loadSound('assets/noteD.mp3')
+  noteE = loadSound('assets/noteE.mp3')
+  noteF = loadSound('assets/noteF.mp3')
+  noteG = loadSound('assets/noteG.mp3')
 }
 
-// setup() waits untill preload() is done //
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  fill(245, 123, 158);
+  textSize(50);
 }
 
 function draw(){
   background(125);
-  keyboardnotes();
 }
 
 function windowResized(){
   setup();
 }
 
-// corresponds with a sound of the piano and shows the note pressed on the screen
-function keyboardnotes(){
-  if (keyIsPressed);{
-    if (key === 'a');{
-      noteA.play();
-      text(key, 33, 65);
-    }
-    if (key === 'b');{
-      noteB.play();
-      text(key, 33, 65);
-    }
-    if (key === 'c');{
-      noteC.play();
-      text(key, 33, 65);
-    }
-    if (key === 'd');{
-      noteD.play();
-      text(key, 33, 65);
-    }
-    if (key === 'e');{
-      noteE.play();
-      text(key, 33, 65);
-    }
-    if (key === 'f');{
-      noteF.play();
-      text(key, 33, 65);
-    }
-    if (key === 'g');{
-      noteG.play();
-      text(key, 33, 65);
-    }
+// corresponds with a sound of the piano
+function keyTyped(){
+  if (key === 'a' ){
+    noteA.setVolume(0.5)
+    noteA.play();
+    text(key, 50, 50)
+  }else if(key === 'b' ){
+    noteB.setVolume(0.5)
+    noteB.play();
+  }
+  else if(key === 'c' ){
+    noteC.setVolume(0.5)
+    noteC.play();
+  }else if(key === 'd' ){
+    noteD.setVolume(0.4)
+    noteD.play();
+  }
+  else if(key === 'e' ){
+    noteE.setVolume(0.5)
+    noteE.play();
+  }
+  else if(key === 'f' ){
+    noteF.setVolume(0.25)
+    noteF.play();
+  }
+  else if(key === 'g' ){
+    noteG.setVolume(0.10)
+    noteG.play();
   }
 }
